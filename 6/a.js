075f272d7a5479f91528c97693f5dd0963ce5c58
Gustav Tonér <gustav.toner@gmail.com>
2020-12-06 10:39:50 +0100

    Day 6

---
+++ 6/a.js
@@ -0,0 +1,16 @@
+fs = require('fs');
+const groups = fs.readFileSync('input.txt').toString().trimEnd().split("\n\n");
+let result = 0;
+for(let g of groups) {
+    const dict = {};
+    for(let a of g.split("\n")) {
+        for(let c of [...a]) {
+            dict[c] = dict[c] == undefined ? 1 : ++dict[c];
+        }
+    }
+    result += Object.keys(dict).length;
+    //console.log(dict);
+    //console.log("-");
+}
+
+console.log(result);
---
+++ 6/b.js
@@ -0,0 +1,17 @@
+fs = require('fs');
+const groups = fs.readFileSync('input.txt').toString().trimEnd().split("\n\n");
+let result = 0;
+for(let g of groups) {
+    const dict = {};
+    const answers = g.split("\n")
+    for(let a of answers) {
+        for(let c of [...a]) {
+            dict[c] = dict[c] == undefined ? 1 : ++dict[c];
+        }
+    }
+    for(var key in dict) {
+        result += dict[key] == answers.length ? 1 : 0;
+    }
+}
+
+console.log(result);
